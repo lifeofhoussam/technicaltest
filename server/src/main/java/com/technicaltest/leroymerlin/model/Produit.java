6abf2abfdb08ea202ddfad9afad0687e8c2d46d3
Houssam <houssam.loukili.etu@univ-lille.fr>
2024-08-11 22:37:54 +0200

    product model

---
+++ server/src/main/java/com/technicaltest/leroymerlin/model/Produit.java
@@ -0,0 +1,34 @@
+package com.technicaltest.leroymerlin.model;
+
+public class Produit {
+
+    private Long id;
+
+    private String nom;
+
+    private Double prix;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(Double prix) {
+        this.prix = prix;
+    }
+}
