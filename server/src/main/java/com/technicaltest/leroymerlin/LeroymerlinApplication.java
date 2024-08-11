a8dc746dfba78e518a4bc6c349438633d216f4bd
Houssam <houssam.loukili.etu@univ-lille.fr>
2024-08-11 02:50:57 +0200

    spring initializr

---
+++ server/src/main/java/com/technicaltest/leroymerlin/LeroymerlinApplication.java
@@ -0,0 +1,13 @@
+package com.technicaltest.leroymerlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeroymerlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeroymerlinApplication.class, args);
+	}
+
+}
